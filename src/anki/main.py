442819f1e41028e4e9c7e1f2313ca4bf622f21e9
Igor Bocharov <igor.bocharov56@yandex.ru>
2026-03-29 23:06:40 +0500

    feat(main): добавить поддержку аргументов командной строки и requests
    - Обновлён main.py для работы с аргументами CLI через argparse
    - Добавлен параметр --source для указания пути к файлу со словами
    - Добавлен пакет requests в зависимости проекта

--- src/anki/main.py
+++ src/anki/main.py
@@ -1,10 +1,58 @@
+import argparse
+import pathlib
+
 from anki.anki import Anki
-from anki.loader import TextFileLoader
+from anki.loader import (
+    BaseFileLoader, TextFileLoader, TSVFileLoader, JsonFileLoader
+)
 from anki.ui import TextUI
 
 
+def get_loader(source: str) -> BaseFileLoader:
+    """
+    Выбирает реализацию загрузчика в зависимости от расширения файла.
+
+    Args:
+        source (str): Путь к файлу для получения слов.
+
+    Returns:
+        BaseFileLoader: Экземпляр класса загрузчика соответствующего типа.
+
+    Raises:
+        ValueError: Если расширение файла не поддерживается.
+    """
+
+    loaders = {
+        '.txt': TextFileLoader,
+        '.tsv': TSVFileLoader,
+        '.json': JsonFileLoader
+    }
+
+    file_path = pathlib.Path(source)
+
+    try:
+        # suffix возвращает расширение файла
+        loader = loaders[file_path.suffix]
+        return loader(file_path=str(file_path))
+    except KeyError:
+        raise ValueError(f'Неизвестный тип источника слов: {source}')
+
+
 def main():
-    loader = TextFileLoader()
+    # Создали объект парсера аргументов командной строки.
+    parser = argparse.ArgumentParser(prog='anki')
+
+    # Добавили новый аргумент.
+    parser.add_argument(
+        '--source', default='./words.txt',
+        help='Путь до источника со словами',
+        metavar='SOURCE_PATH',
+    )
+
+    # Распарсили аргументы командной строки.
+    args = parser.parse_args()
+
+    loader = get_loader(args.source)
 
     anki = Anki(words=loader.load_words())
 
